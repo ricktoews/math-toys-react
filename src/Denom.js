233c22627b0d8bc28341d0a5bb41dbf4b9d5fc08
Rick Toews <rick.toews@gmail.com>
2019-07-11 23:18:35 -0700

    Clean up display a little. Make expansions clickable.

--- src/Denom.js
+++ src/Denom.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import prep from './denom-helper';
+import './Denom.css';
 
 function fetchDenom(denom) {
   var url = 'http://localhost:8080/denom_byexpansion/' + denom;
@@ -32,26 +33,15 @@ class Numerator extends Component {
   }
 }
 
-class Denom extends Component {
+class Expansion extends Component {
   constructor(props) {
     super(props);
-    this.denom = props.match.params.denom;
-    this.state = { denom: null, info: {}, expansions: {}, forDisplay: null };
     this.setNumerator = this.setNumerator.bind(this);
-  }
-
-  componentDidMount() {
-    let denom = this.denom;
-    fetchDenom(denom).then(res => {
-      res.expansions.sort((a, b) => a.numerator - b.numerator);
-      let expansions = res.expansions;
-      expansions.unshift({});
-      this.setState({ denom: denom, info: res, expansions });
-    });
+    this.handleClick = this.handleClick.bind(this);
   }
 
   formatNumeratorList(numerators) {
-    let expansions = this.state.expansions;
+    let expansions = this.props.expansions;
     let formatted = (
       <div className="numerator">
       {numerators.map(numerator => (
@@ -82,30 +72,71 @@ class Denom extends Component {
   }
 
   setNumerator(expansionData) {
-    let fraction = <span className="fraction"><span className="numerator">{expansionData.numerator}</span> / <span className="denominator">{this.state.denom}</span></span>;
+    let fraction = <span className="fraction"><span className="numerator">{expansionData.numerator}</span> / <span className="denominator">{this.props.denom}</span></span>;
     let forDisplay = this.expansionForDisplay(expansionData);
-    this.setState({ fraction, forDisplay });
+    this.props.updateDisplay({ fraction, forDisplay });
+  }
+
+  handleClick(e) {
+    let thisEl = e.target;
+    let numeratorsEl = thisEl.nextSibling;
+    let els = Array.from(document.getElementsByClassName('numerators'));
+    els.forEach(el => { el.classList.remove('show-numerators'); el.classList.add('hide-numerators'); });
+    numeratorsEl.classList.remove('hide-numerators');
+    numeratorsEl.classList.add('show-numerators');
+    console.log('click', this, e.target.nextSibling);
   }
 
   render() {
-    const { denom, info } = this.state;
+    const g = this.props.item;
+    return (
+      <div key={g.expansion}>
+      <h3 onClick={this.handleClick}>{g.expansion}</h3>
+        <div className="numerators hide-numerators">
+        {this.formatNumeratorList(g.numerators)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Denom extends Component {
+  constructor(props) {
+    super(props);
+    this.denom = props.match.params.denom;
+    this.state = { denom: null, info: {}, expansions: {}, forDisplay: null };
+    this.updateDisplay = this.updateDisplay.bind(this);
+  }
+
+  componentDidMount() {
+    let denom = this.denom;
+    fetchDenom(denom).then(res => {
+      res.expansions.sort((a, b) => a.numerator - b.numerator);
+      let expansions = res.expansions;
+      expansions.unshift({});
+      this.setState({ denom: denom, info: res, expansions });
+    });
+  }
+
+  updateDisplay(stateVars) {
+    this.setState({ ...stateVars });
+  }
+
+
+  render() {
+    const { denom, info, expansions } = this.state;
     if (!denom) {
       return <div className="content"><h1>Denom page</h1></div>
 	} else {
       return (
       <div className="content">
-        <Link to="/">Home</Link>
-        <h1>Denom page</h1>
         <div className="flex-container">
           <div className="left-side">
             <h2>{denom}ths</h2>
             <p>{info.groupCount} for fractions having a denominator of {denom}:</p>
             <p>The numerators for these are:</p>
             {info.groups.map(g => (
-              <div key={g.expansion}>
-              <h3>{g.expansion}</h3>
-              {this.formatNumeratorList(g.numerators)}
-              </div>
+              <Expansion key={g.expansion} updateDisplay={this.updateDisplay} item={g} expansions={expansions} denom={denom} />
             ) )}
           </div>
           <div className="right-side">
