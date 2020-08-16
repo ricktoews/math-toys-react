77ac7c9032001113fe368eecca79a266ce782102
Rick Toews <rick.toews@gmail.com>
2020-08-16 13:35:52 -0700

    Attempt at build for new arithmo site.

---
+++ arithmo.toewsweb.net/precache-manifest.5e2300310ea1f3f356be2a1266209b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c076d79cb52fb4be503a0669157754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52c8e88303a715c26458",
+    "url": "/static/css/2.614afd85.chunk.css"
+  },
+  {
+    "revision": "7cfc69ea60e4a74e3c28",
+    "url": "/static/css/main.868d74b5.chunk.css"
+  },
+  {
+    "revision": "52c8e88303a715c26458",
+    "url": "/static/js/2.468cb6f5.chunk.js"
+  },
+  {
+    "revision": "7cfc69ea60e4a74e3c28",
+    "url": "/static/js/main.a127c934.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
